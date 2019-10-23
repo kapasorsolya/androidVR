ac68c4c2d1da3128a2ac26327295ec0655510ced
Mille János <43989047+MilleJanos@users.noreply.github.com>
2019-10-23 17:19:43 +0300

    Coin & pickup

---
+++ VR App/Assets/CoinScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(90 * Time.deltaTime, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<PlayerScript>().points++;
+        if (other.name == "FPSController")
+        {
+            //Add 1 to points;
+            other.GetComponent<PlayerScript>().points++;
+            // Destroy object
+            Destroy(gameObject);
+        }
+    }
+}
